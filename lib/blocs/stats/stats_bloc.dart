95fdf298554d2339400e8add157e8d6666ed50f0
S <30815731+slimlime@users.noreply.github.com>
2021-05-05 12:53:17 +1000

    chore(/,pubspec): add sample code, update pubspec bloc flutter_bloc deps
    To check to what extent the sample works out of the box
    
    BREAKING CHANGE: Likely does not work or requires migration for first steps before subsequent
    recreating a new prototype project with given meta details

---
+++ lib/blocs/stats/stats_bloc.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_firestore_todos/blocs/blocs.dart';
+
+class StatsBloc extends Bloc<StatsEvent, StatsState> {
+  StreamSubscription _todosSubscription;
+
+  StatsBloc({TodosBloc todosBloc})
+      : assert(todosBloc != null),
+        super(StatsLoading()) {
+    _todosSubscription = todosBloc.stream.listen((state) {
+      if (state is TodosLoaded) {
+        add(UpdateStats(state.todos));
+      }
+    });
+  }
+
+  @override
+  Stream<StatsState> mapEventToState(StatsEvent event) async* {
+    if (event is UpdateStats) {
+      int numActive =
+          event.todos.where((todo) => !todo.complete).toList().length;
+      int numCompleted =
+          event.todos.where((todo) => todo.complete).toList().length;
+      yield StatsLoaded(numActive, numCompleted);
+    }
+  }
+
+  @override
+  Future<void> close() {
+    _todosSubscription?.cancel();
+    return super.close();
+  }
+}
