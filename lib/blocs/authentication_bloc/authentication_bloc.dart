457af16a2f632fe48a25c3356f0e648e9559c42d
s <82124333+webstoreportal@users.noreply.github.com>
2021-05-06 13:48:01 +1000

    feat(web,pubspec,authentication): configure for flutter web firebase
    Working todos read and write. Stats are endlessly loading though.
    Configure through webGUI Firebase Console

--- lib/blocs/authentication_bloc/authentication_bloc.dart
+++ lib/blocs/authentication_bloc/authentication_bloc.dart
@@ -1,14 +1,15 @@
 import 'dart:async';
+
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-import 'package:user_repository/user_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_firestore_todos/blocs/authentication_bloc/bloc.dart';
+import 'package:user_repository/user_repository.dart';
 
 class AuthenticationBloc
     extends Bloc<AuthenticationEvent, AuthenticationState> {
   final UserRepository _userRepository;
 
-  AuthenticationBloc({required UserRepository userRepository})
+  AuthenticationBloc({required FirebaseUserRepository userRepository})
       : assert(userRepository != null),
         _userRepository = userRepository,
         super(Uninitialized());
@@ -28,9 +29,18 @@ class AuthenticationBloc
       if (!isSignedIn) {
         await _userRepository.authenticate();
       }
+
       final userId = await _userRepository.getUserId();
       yield Authenticated(userId);
-    } catch (_) {
+    } on FirebaseAuthException catch (exc) {
+      print(
+        'FirebaseAuthException ${exc.toString()}',
+      );
+
+      return;
+    } catch (errception) {
+      print(errception.toString());
+
       yield Unauthenticated();
     }
   }
